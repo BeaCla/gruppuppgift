ffc165b15e2e6d0e56b24e297212a52be4b556f4
nilin <nilin@DESKTOP-QDRD43M>
2019-12-16 13:56:43 +0100

    Ändrat i Modellen

--- grupparbete/src/application/model/Competition.java
+++ grupparbete/src/application/model/Competition.java
@@ -3,5 +3,21 @@ package application.model;
 import java.io.Serializable;
 
 public class Competition  implements Serializable{
+	
+	private Competitors competitors = null; 
+	
+	public Competition() {
+		competitors = new Competitors();
+	}
 
+	
+	public void name() {
+		competitors.add(new Competitor());
+	}
+	
+	public Competitors getMassStart() {
+		
+		return null;
+		
+	}
 }
---
+++ grupparbete/src/application/model/Competitors.java
@@ -0,0 +1,18 @@
+package application.model;
+
+import java.util.ArrayList;
+
+public class Competitors extends ArrayList<Competitor> {
+
+	public void addCompetitor(Competitor c) {
+
+	}
+	
+	public void deleteCompetitor(Integer platsIListan) {
+
+	}
+	
+	public void deleteAllCompetitor() {
+
+	}
+}
