d8e8373e9bb06a27c6057ff8ae14b7d42df7d214
nilin <nilin@DESKTOP-QDRD43M>
2019-12-16 14:02:18 +0100

    Lagt till serialVersionUID till Competitors

--- grupparbete/src/application/model/Competitors.java
+++ grupparbete/src/application/model/Competitors.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class Competitors extends ArrayList<Competitor> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2877483646104013301L;
+
 	public void addCompetitor(Competitor c) {
 
 	}
