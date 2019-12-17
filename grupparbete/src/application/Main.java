640cd5143909b0e1fc2cdba19ae3d8ba91706d04
nilin <nilin@DESKTOP-QDRD43M>
2019-12-17 09:28:10 +0100

    Städat main, tagit bort borderpane som var bortkommenterat

--- grupparbete/src/application/Main.java
+++ grupparbete/src/application/Main.java
@@ -4,15 +4,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			SkiComp root = new SkiComp();
-			//BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,700,500);
 			scene.getStylesheets().add(getClass().getResource("resources/application.css").toExternalForm());
 			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("resources/cross-country-skiing.png")));
